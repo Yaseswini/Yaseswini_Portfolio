7e3bf2495972dc430ac0682439fdddf8eaaab954
Yaseswini  N <yaseswini.1404@gmail.com>
2025-03-17 20:28:56 -0400

    updated cv with education and work

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -1,11 +1,32 @@
 export const experiences = [
 	{
 		company: 'University of Virginia',
-		time: '2017 Feb - 2018 Aug',
+		time: '2024 Jan - Present',
+		title: 'Research Scientist,Bioinformatics & Computational Biology',
+		location: 'Remote',
+		description: '',
+	},
+	{
+		company: 'University of Virginia',
+		time: '2017 Mar - 2018 Aug',
 		title: 'Bioinformatics Analyst',
 		location: 'Charlottesville',
 		description: '',
 	},
+	{
+		company: 'Weill Cornell Medicine',
+		time: '2015 Oct - 2017 Feb',
+		title: 'Bioinformatics Analyst',
+		location: 'New York',
+		description: '',
+	},
+	{
+		company: 'Indiana University - Purdue University(IUPUI), Indianapolis',
+		time: '2013 Aug - 2015 May',
+		title: 'Bioinformatician',
+		location: 'Indianapolis',
+		description: '',
+	}
 	// {
 	// 	company: 'Radium Institute (Institut du Radium)',
 	// 	time: '1914 - 1934',
@@ -17,12 +38,26 @@ export const experiences = [
 
 export const education = [
 	{
-		school: '',
-		time: '',
-		degree: '',
-		location: '',
+		school: 'University of Virginia',
+		time: '2018 Aug - 2023 December',
+		degree: 'Doctor of Philosophy(PhD)',
+		location: 'Charlottesville,Virginia',
 		description: '',
 	},
+	{
+		school: 'University of Hyderabad',
+		time: '2011 Aug - 2013 May',
+		degree: 'Masters of Technology(M.Tech)',
+		location: 'Hyderabad,India',
+		description: '',
+	},
+	{
+		school: 'Sreenidhi Institute of Science and Technology (JNTU affiliated)',
+		time: '2007 Aug - 2011 Apr',
+		degree: 'Bachelor of Technology(B.Tech)',
+		location: 'Hyderabad,India',
+		description: '',
+	}
 	// {
 	// 	school: 'University of Paris',
 	// 	time: '1891 - 1895',
@@ -32,32 +67,32 @@ export const education = [
 	// },
 ];
 
-export const skills = [
-	{
-		title: '',
-		description: '',
-	},
-	// {
-	// 	title: 'Experimental Techniques',
-	// 	description: 'Spectroscopy, Isolation of Radioactive Elements, Radiation Measurement',
-	// },
-];
+// export const skills = [
+// 	{
+// 		title: '',
+// 		description: '',
+// 	},
+// 	// {
+// 	// 	title: 'Experimental Techniques',
+// 	// 	description: 'Spectroscopy, Isolation of Radioactive Elements, Radiation Measurement',
+// 	// },
+// ];
 
-export const publications = [
-	{
-		title: '',
-		authors: '',
-		journal: '',
-		time: '',
-		link: '',
-		abstract: '',
-	},
-	// {
-	// 	title: 'The Radiation of Uranium Compounds',
-	// 	authors: 'Marie Curie',
-	// 	journal: 'Comptes Rendus de l’Académie des Sciences',
-	// 	time: '1898',
-	// 	link: '#',
-	// 	abstract: 'Early research leading to the identification of uranium’s radioactive properties.',
-	// },
-];
+// export const publications = [
+// 	{
+// 		title: '',
+// 		authors: '',
+// 		journal: '',
+// 		time: '',
+// 		link: '',
+// 		abstract: '',
+// 	},
+// 	// {
+// 	// 	title: 'The Radiation of Uranium Compounds',
+// 	// 	authors: 'Marie Curie',
+// 	// 	journal: 'Comptes Rendus de l’Académie des Sciences',
+// 	// 	time: '1898',
+// 	// 	link: '#',
+// 	// 	abstract: 'Early research leading to the identification of uranium’s radioactive properties.',
+// 	// },
+// ];
